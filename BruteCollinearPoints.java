4df03e9f6ddea6b95da3cc3566e3021691542bac
Zhang Yi <zhangyi_1357@sjtu.edu.cn>
2022-03-08 10:27:23 +0800

    initial commit with Point finished

---
+++ BruteCollinearPoints.java
@@ -0,0 +1,25 @@
+/* *****************************************************************************
+ *  Name:        Zhang Yi
+ *  Date:        2022/03/08
+ *  Description: BruteCollinearPoints detection
+ **************************************************************************** */
+
+public class BruteCollinearPoints {
+    public BruteCollinearPoints(Point[] points) {  // finds all line segments containing 4 points
+        if (points == null)
+            throw new IllegalArgumentException();
+        
+    }
+
+    public int numberOfSegments() { // the number of line segments
+        return 0;
+    }
+
+    public LineSegment[] segments() { // line segments
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
